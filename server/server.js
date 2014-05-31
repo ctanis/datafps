6ccd31095b6a8981f5b75d28069a76df38e4a589
Forrest Pruitt <fpruitt0922@gmail.com>
2014-05-31 13:39:40 -0400

    adding simple client/server echo

--- server/server.js
+++ server/server.js
@@ -1,192 +1,52 @@
-/*---------------------------------------------------------
-newCloudServer.js
-A Pass-through server for Engage3D's Kinect Video
-Conferencing App. Creates two servers to listen for
-messages from both the Viewer and the Kincet.
-Author: Forrest Pruitt
----------------------------------------------------------*/
-/* Note
-For Memory Leaks, Memwatch is great:
-var memwatch = require('memwatch');
-var hd = new memwatch.HeapDiff();
-~CODE TO TEST GOES HERE~
-//var diff = hd.end();
-*/
-
+#!/usr/bin/env node
 var WebSocketServer = require('websocket').server;
 var http = require('http');
-var http2 = require('http');
-// var connections = [];
-var kinect_connections = [];
-var viewer_connections = [];
-var kinectServer = http.createServer(function(request, response) {
-    // process HTTP request. Since we're writing just WebSockets server
-    // we don't have to implement anything.
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
 });
-var viewerServer = http2.createServer(function(request, response) {
-    // process HTTP request. Since we're writing just WebSockets server
-    // we don't have to implement anything.
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
 });
 
-kinectServer.listen(9000, function() { });
-viewerServer.listen(9001, function() { });
-
-// create the server
-
-wsKinectServer = new WebSocketServer({
-    httpServer: kinectServer,
-    maxReceivedFrameSize: 64*1024*1024, // 64MiB
-    maxReceivedMessageSize: 64*1024*1024, // 64MiB
-    protocol: 'of-protocol'
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
 });
 
-wsViewerServer = new WebSocketServer({
-    httpServer: viewerServer,
-    maxReceivedFrameSize: 64*1024*1024, // 64MiB
-    maxReceivedMessageSize: 64*1024*1024, // 64MiB
-    protocol: 'of-protocol'
-});
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
 
-var kinectConnection;
-var viewerConnection;
-var kinectConnected=false;
-var viewerConnected=false;
-console.log('Listening on port 9000 for the Kinect');
-console.log('Listening on port 9001 for the Viewer');
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
 
-//---------------------------------------------
-// Kinect Code
-//---------------------------------------------
-wsKinectServer.on('request', function(request)
-{
-    kinectConnection = request.accept('of-protocol', request.origin);
-    kinect_connections.push(kinectConnection);
-console.log('Connected to kinect client.');
-    kinectedConnected=true;
-    // This is the most important callback for us, we'll handle
-    // all messages from users here.
-
-    kinectConnection.on('message', function(message)
-    {
-        if (message.type === 'utf8')
-        {
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
         }
-
-        else if (message.type === 'binary')
-        {
-            //console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            if(viewerConnected)
-            {
-                viewer_connections.forEach(function(destination)
-                {
-                    if(destination != kinectConnection)
-                        destination.sendBytes(message.binaryData);
-                });
-            }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
         }
-
-});
-
-    kinectConnection.on('close', function(connection)
-    {
-        console.log('Closing connection to Kinect...');
-
-        kinect_connections.forEach(function(connection)
-        {
-            if(kinectConnection.socket._peername == connection.socket._peername)
-            {
-                console.log("Closing connection with label: ");
-                console.log(connection.socket._peername);
-                kinect_connections.splice(connection,1);
-            }
-        });
-
-        kinectConnected = false;
-        kinectConnection = null;
-
-        console.log('Closed connection to Kinect.');
-        var numConnections = 0;
-        kinect_connections.forEach(function(connection)
-        {
-            numConnections++;
-        }
-        );
-        console.log('Open Connections: ' + numConnections );
-
     });
-
-});
-
-//-----------------------------------------------
-// Viewer Code
-//----------------------------------------------
-
-//Set up the connection from this server to the viewer.
-wsViewerServer.on('request', function(request)
-{
-    
-    clientConnection = request.accept(null, request.origin);
-    console.log(clientConnection.socket._peername)
-    viewer_connections.push(clientConnection);
-    console.log('Connected to viewer client.');
-    viewerConnected = true;
-
-    //Here we are handling messages from the viewer to the Kinect.
-    //This is currently (March 2013) used primarily for throttling the
-    //Stream, ie setting the framerate.
-    clientConnection.on('message', function(message)
-    {
-        if (message.type === 'utf8')
-        {
-            kinect_connections.forEach(function(destination)
-            {
-                if(destination != clientConnection)
-                    destination.sendUTF(message.utf8Data);
-            });
-        }
-        else if (message.type === 'binary')
-        {//probably not sending binary this way
-        // destination.sendBytes(message.binaryData);
-        // connections.forEach(function(destination)
-        // {
-        // //
-        // if(destination != clientConnection)
-        // destination.sendBytes(message.binaryData);
-        
-        // });
-            
-        }
-        console.log(message);
-    });
-
-    clientConnection.on('close', function(connection)
-    {
-        console.log('Closing connection to viewer.');
-        viewer_connections.forEach(function(connection)
-            {
-                if(clientConnection.socket._peername == connection.socket._peername)
-                {
-                    console.log("Closing connection with label: ");
-                    console.log(connection.socket._peername);
-                    viewer_connections.splice(connection,1);
-                }
-            }
-        );
-
-        console.log('Closed connection to viewer.');
-        var numConnections = 0;
-        viewer_connections.forEach(function(connection)
-        {
-            numConnections++;
-        }
-        );
-        console.log('Open Connections: ' + numConnections );
-
-
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
-
-
-//var diff = hd.end();
-//console.log(diff);

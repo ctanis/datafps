3329416063d5215bbcb89551285500a83e693056
Craig Tanis <craig-tanis@utc.edu>
2014-05-31 23:22:28 -0400

    drawing shapes sent from server

--- client/datafps.js
+++ client/datafps.js
@@ -35,11 +35,50 @@ DataFPS = function() {
             console.log("got a message");
             // console.log("msg: " + message);
 
-            console.log(message);
+            // console.log(message);
             var foo = jQuery.parseJSON(message.data);
-            // console.log(foo.vertices.length);
+            console.log(foo);
             // console.log("goodbye");
 
+            var model = new THREE.Geometry();
+            // model.vertices = foo.vertices;
+            model.vertices = [];
+            model.faces = [];
+
+            for (var v=0; v<foo.vertices.length; v++) {
+                var a = new THREE.Vector3(foo.vertices[v][0],
+                                          foo.vertices[v][1],
+                                          foo.vertices[v][2]);
+                console.log(a);
+                model.vertices.push(a);
+            }
+
+            for (var v=0; v<foo.connectivity.length; v++) {
+                console.log("blah");
+                console.log("hi "  + foo.connectivity[v]);
+                console.log(foo.connectivity[v]);
+
+                var b = new THREE.Face3(foo.connectivity[v][0],
+                                        foo.connectivity[v][1],
+                                        foo.connectivity[v][2]);
+
+                model.faces.push(b);
+            }
+            
+            
+            // model.faces = foo.connectivity;
+            
+
+            // tmp
+            var material = new THREE.MeshBasicMaterial( {color: 0xff0000, side:THREE.DoubleSide, wireframe: true } );
+            // var model = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
+            
+            var mesh = THREE.SceneUtils.createMultiMaterialObject( model, [
+                new THREE.MeshNormalMaterial( { color: 0xffffff} ),
+                new THREE.MeshBasicMaterial( { color: 0x222222, wireframe: true})
+            ]);
+
+            scene.add(mesh);
         }
 
 
@@ -57,23 +96,23 @@ DataFPS = function() {
 
         var controls = new THREE.OrbitControls(camera);
 
-        // tmp
-        var material = new THREE.MeshBasicMaterial( {color: 0xff0000, side:THREE.DoubleSide, wireframe: true } );
-        var model = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
+        //         // tmp
+        //         var material = new THREE.MeshBasicMaterial( {color: 0xff0000, side:THREE.DoubleSide, wireframe: true } );
+        //         var model = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
 
-        var mesh = THREE.SceneUtils.createMultiMaterialObject( model, [
-            new THREE.MeshNormalMaterial( { color: 0xffffff} ),
-            new THREE.MeshBasicMaterial( { color: 0x222222, wireframe: true} )
+        //         var mesh = THREE.SceneUtils.createMultiMaterialObject( model, [
+        //             new THREE.MeshNormalMaterial( { color: 0xffffff} ),
+        //             new THREE.MeshBasicMaterial( { color: 0x222222, wireframe: true} )
 
-]);
+        // ]);
 
-//        scene.add(new THREE.Mesh(model, material));
-        scene.add(mesh);
-        var plane = new THREE.PlaneGeometry(10, 10 );
+        //        scene.add(new THREE.Mesh(model, material));
+        // scene.add(mesh);
+        // var plane = new THREE.PlaneGeometry(10, 10 );
         // var v = new THREE.Vector3(0, -1, 0);
         // plane.translate(v);
 
-        scene.add(new THREE.Mesh(plane , new THREE.MeshBasicMaterial({color: 0x00ff00, side:THREE.DoubleSide})));
+        // scene.add(new THREE.Mesh(plane , new THREE.MeshBasicMaterial({color: 0x00ff00, side:THREE.DoubleSide})));
 
     }
 
@@ -86,7 +125,9 @@ DataFPS = function() {
             camera.lookAt(scene.position);
 
             renderer.render(scene, camera);
-	    window.requestAnimationFrame(animate, renderer.domElement);            
+
+            // console.log(renderer.domElement);
+            window.requestAnimationFrame(animate, renderer.domElement);            
         }
 
 
